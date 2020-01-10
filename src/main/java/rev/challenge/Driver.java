ea9e1737977ae5c64abcb8f44a271138ee432765
Tannilen Reagan <tannilen.reagan@gmail.com>
2020-01-10 10:28:25 -0500

    Leaderboard now correctly retaining high scores.

--- src/main/java/rev/challenge/Driver.java
+++ src/main/java/rev/challenge/Driver.java
@@ -7,7 +7,6 @@ import rev.challenge.instructions.Menu;
 
 public class Driver {
 	public static void main(String[] args) {
-		Leaderboard.initialize();
 		Leaderboard.load();
 		Scanner scan = new Scanner(System.in);
 		Menu.logo();
